da6b70ed8e08721bba0bba508bb64d0c6d9f9749
LEONARDO AUGUSTO DO PRADO <leonardo.aprado@senacsp.edu.br>
2019-08-21 22:30:42 -0300

    barras moveis, botão start

---
+++ EventoSGA/Assets/Scripts/MovBarra.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovBarra : MonoBehaviour
+{
+    public float velocidade = 0.05f;
+    public float velcima = 0.05f;
+    public float velbaixo = 0.05f;
+    public GameObject cima;
+    public GameObject baixo;
+
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * velocidade);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == cima)
+        {
+            velocidade = velcima * -1;
+        }
+        if (other.gameObject == baixo)
+        {
+            velocidade = velbaixo * -1;
+        }
+
+    }
+}
---
+++ EventoSGA/Assets/Scripts/MovBarraLateral.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovBarraLateral : MonoBehaviour
+{
+    public float velocidade = 0.05f;
+    public float velDireita = 0.05f;
+    public float velEsquerda = 0.05f;
+    public GameObject Direita;
+    public GameObject Esquerda;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * velocidade);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == Direita)
+        {
+            velocidade = velDireita * -1;
+        }
+        if (other.gameObject == Esquerda)
+        {
+            velocidade = velEsquerda * -1;
+        }
+
+    }
+}
