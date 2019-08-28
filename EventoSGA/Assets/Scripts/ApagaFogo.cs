6a7cd13024b091399956c7215b9ccf84fc59873d
LEONARDO AUGUSTO DO PRADO <leonardo.aprado@senacsp.edu.br>
2019-08-27 22:11:55 -0300

    inimigo e tiro corrigidos

---
+++ EventoSGA/Assets/Scripts/ApagaFogo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class ApagaFogo : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject TiroAgua;
+   
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "TiroAgua")
+        {
+            Destroy(this.gameObject);
+            Destroy(this.TiroAgua);
+
+        }
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
